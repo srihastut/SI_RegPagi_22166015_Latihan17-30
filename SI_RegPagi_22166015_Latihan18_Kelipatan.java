0fdd032efb789e0b611aada81b36ce705e957ccc
srihastut <149176809+srihastut@users.noreply.github.com>
2023-10-29 09:01:59 +0700

    Add files via upload

---
+++ SI_RegPagi_22166015_Latihan18_Kelipatan.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi_22166015_latihan18_kelipatan;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ * NAMA : Sri Hastuti
+ * KELAS : PBO1
+ * NIM : 22166015
+ * DESKRIPSI PROGRAM : Kelipatan
+ */
+public class SI_RegPagi_22166015_Latihan18_Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TOpublic static void main(String[] args) {
+        // Loop untuk menampilkan bilangan kelipatan 3 dan 5
+        for (double i = 3.5; i <= 35; i += 0.5) {
+            if (i % 3.5 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
