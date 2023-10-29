9f992f79bd71d921dd29f1b0efd3b8fff03f3ffc
srihastut <149176809+srihastut@users.noreply.github.com>
2023-10-29 09:11:31 +0700

    Add files via upload

---
+++ SI_RegPagi_22166015_Latihan27_HurufBesarKecil.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi_22166015_latihan27_huruf.besar.kecil;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ * Nama : Sri Hastuti
+ * KELAS : PBO1
+ * NIM : 22166015
+ * DESKRIPSI PROGRAM : Huruf Besar Kecil
+ */
+public class SI_RegPagi_22166015_Latihan27_HurufBesarKecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan Kalimat: ");
+        String sentence = input.nextLine();
+
+        String uppercase = sentence.toUpperCase();
+        String lowercase = sentence.toLowerCase();
+
+        System.out.println("----Hasil Formatting----");
+        System.out.println("Huruf Besar : " + uppercase);
+        System.out.println("Huruf Kecil : " + lowercase);
+    }
+}
