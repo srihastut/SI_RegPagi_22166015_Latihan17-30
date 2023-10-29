73f70b9db58742c985631d03e033b0167813db4f
srihastut <149176809+srihastut@users.noreply.github.com>
2023-10-29 09:09:45 +0700

    Add files via upload

---
+++ SI_RegPagi_22166015_Latihan25_EjaanNama.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi_22166015_latihan25_ejaan.nama;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ * Nama : Sri Hastuti
+ * KELAS : PBO1
+ * NIM : 22166015
+ * DESKRIPSI PROGRAM : Ejaan Nama
+ */
+public class SI_RegPagi_22166015_Latihan25_EjaanNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nama depan anda untuk di eja: ");
+        String name = input.nextLine();
+
+        System.out.println("Ejaan untuk \"" + name + "\" adalah :");
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println("Huruf ke-" + (i+1) + ": " + name.charAt(i));
+        }
+    }
+}
