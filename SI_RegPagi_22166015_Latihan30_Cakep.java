3dd673adbfcba8340c661a41ff0ff7ede554c3a5
srihastut <149176809+srihastut@users.noreply.github.com>
2023-10-29 09:14:11 +0700

    Add files via upload

---
+++ SI_RegPagi_22166015_Latihan30_Cakep.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi_22166015_latihan30_cakep;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ * Nama : Sri Hastuti
+ * KELAS : PBO1
+ * NIM : 22166015
+ * DESKRIPSI PROGRAM : Cakep
+ */
+public class SI_RegPagi_22166015_Latihan30_Cakep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Kamu ngerjain sendiri latihan 17 sampai latihan 30 ini? ");
+        System.out.print("Jawab (Yoi/Enggak): ");
+        String jawab = scanner.next();
+        
+        if (jawab.contains("Yoi")) {
+            System.out.println("Cakep Bener. Good Job");
+        } else {
+            System.out.println("Tetep Cakep sih.");
+            System.out.println("Sing Penting Paham Konsep nye yee.");
+            System.out.println("Keep the code works dude.");
+        }
+  }
+}
+    
